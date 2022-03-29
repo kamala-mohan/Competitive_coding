6e70cc35412393de53be0bedc2f0a112f8f1e2dd
kamala-mohan <69154342+kamala-mohan@users.noreply.github.com>
2022-03-29 11:17:34 +0530

    Add files via upload

---
+++ Graphs/connectedComponentDFS.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<long long>> graph;   //this is an unweighted graph,so we do not use pair in this
+
+void addEdge(int u,int v){
+	graph[u].push_back(v);
+	graph[v].push_back(u);
+}
+void dfsHelper(int src,vector<bool> &visited){
+	visited[src] = true;
+	// cout<<src<<" ";
+	for(int neighbor : graph[src]){
+		if(not visited[neighbor]){
+			dfsHelper(neighbor,visited);
+		}
+	}
+}
+
+int connectedComponent(int src,int v){
+	int component = 0;
+	vector<bool>visited(v,false);
+	for(int  i= 1;i<v;i++){
+		if(not visited[i]){
+			component++;
+			dfsHelper(i,visited);
+		}
+	}
+	return component;
+}
+void dfs(int src,int v){
+	vector<bool>visited(v,false);
+	//for disconnecetd graphs as well
+	for(int i= 1;i<v;i++){
+		if(not visited[i]){
+			dfsHelper(i,visited);
+		}
+	}
+	
+}
+
+int main(){
+	int v,e;
+	cin>>v>>e;
+	graph.resize(v+1);
+	while(e--){
+		int u,v;
+		cin>>u>>v;
+		addEdge(u,v);
+	}
+	cout<<connectedComponent(1,v+1);
+	return 0;
+}
